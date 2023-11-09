70760cdb5e3ff21a9055a0d7842167bb6b5b679a
N1CO-S4X3N <33672311+N1CO-S4X3N@users.noreply.github.com>
2023-11-09 16:38:41 +0200

    Update streamlit_app.py
    Add urllib error import that will be used later.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import pandas
 import requests
 import streamlit
-
 import snowflake.connector
+
+from urllib.error import URLError
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
