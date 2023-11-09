4838d559c521f8764494ffb791fd60979b613943
N1CO-S4X3N <33672311+N1CO-S4X3N@users.noreply.github.com>
2023-11-09 16:50:25 +0200

    Organize
    Break out into function and try catch.
    Stop streamlit after relevant things while trouble shooting

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,17 +24,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+
 # New section to display fruityvice api response
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# Normalize the json response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
 streamlit.header("Fruityvice Fruit Advice!")
-# Display it in a dataframe format
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get infomration.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
