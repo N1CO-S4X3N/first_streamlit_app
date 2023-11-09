af57ebf91d7e975e23ef338c26f6118c824ccb71
N1CO-S4X3N <33672311+N1CO-S4X3N@users.noreply.github.com>
2023-11-09 16:20:26 +0200

    Update streamlit_app.py
    Still, display only if something was added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,4 +42,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_fruit = streamlit.text_input("What fruit would you like to add?")
-streamlit.write("Thanks for adding ", add_fruit)
+if add_fruit:
+  streamlit.write("Thanks for adding ", add_fruit)
